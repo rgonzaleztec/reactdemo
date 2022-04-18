40cf369bd6e5d7cd14b4d72e7b5e949491ea5752
rgonzaleztec <rogelio.rgonzalez@gmail.com>
2022-04-17 20:03:57 -0600

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './style.css';
 import Home from './components/Home';
 import Logueo from './components/Logueo';
 
+import firebaseApp from '../credenciales';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const auth = getAuth(firebaseApp);
+
 export default function App() {
   const [usuarioGlobal, setUsuarioGlobal] = useState(null);
+
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
+      setUsuarioGlobal(usuarioFirebase);
+    } else {
+      setUsuarioGlobal(null);
+    }
+  });
   return <>{usuarioGlobal ? <Home /> : <Logueo />}</>;
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import firebaseApp from '../../credenciales';
+import { getAuth, signOut } from 'firebase/auth';
+import { Container, Button } from 'react-bootstrap';
+
+const auth = getAuth(firebaseApp);
 
 const Home = () => {
-  return <div> Home </div>;
+  return (
+    <Container>
+      <h4>Bienvenido, sesion iniciada</h4>
+      <Button onClick={() => signOut(auth)}> Cerrar sesion </Button>
+    </Container>
+  );
 };
 
 export default Home;
