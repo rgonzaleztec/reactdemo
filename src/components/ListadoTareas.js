863a51425e30ba8f284b0d0acb839ef6c14c4048
rgonzaleztec <rogelio.rgonzalez@gmail.com>
2022-04-18 08:51:26 -0600

    2 files modified

--- src/components/ListadoTareas.js
+++ src/components/ListadoTareas.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Stack, Container, Row, Col, Button } from 'react-bootstrap';
-import firebaseApp from "../../credenciales";
-import {getFirestore, updateDoc, doc } from "firebase/firestore";
+import firebaseApp from '../../credenciales';
+import { getFirestore, updateDoc, doc } from 'firebase/firestore';
 
 const firestore = getFirestore(firebaseApp);
 
 const ListadoTareas = ({ arrayTareas, correoUsuario, setArrayTareas }) => {
-
-async function eliminarTarea(idTareaEliminar) {
-  // creamos un arreglo nuevo con los objetos que no son el que queremos eliminar
-  // Un nuevo arreglo sin un elemento. Esto no lo elimina de firestore
-  const nuevaArrayTareas = arrayTareas.filter((objetoTarea) => objetoTarea.id !== idTareaEliminar);
-  // actualizar la BD 
-  // Buscamos en la coleccion con el documento que hace referencia al correo del usuario para traer los datos
-  const docuRef = doc(firestore,`usuarios/${correoUsuario}`);
-  updateDoc(docuRef, {tareas: [...nuevaArrayTareas]});
-  //actualizamos el estado esta funcion no tiene await porque no tenemos que esperar respuesta
-  setArrayTareas(nuevaArrayTareas);
-
-
-}
+  async function eliminarTarea(idTareaEliminar) {
+    // creamos un arreglo nuevo con los objetos que no son el que queremos eliminar
+    // Un nuevo arreglo sin un elemento. Esto no lo elimina de firestore
+    const nuevaArrayTareas = arrayTareas.filter(
+      (objetoTarea) => objetoTarea.id !== idTareaEliminar
+    );
+    // actualizar la BD
+    // Buscamos en la coleccion con el documento que hace referencia al correo del usuario para traer los datos
+    const docuRef = doc(firestore, `usuarios/${correoUsuario}`);
+    updateDoc(docuRef, { tareas: [...nuevaArrayTareas] });
+    //actualizamos el estado esta funcion no tiene await porque no tenemos que esperar respuesta
+    setArrayTareas(nuevaArrayTareas);
+  }
 
   return (
     <Container>
@@ -30,10 +29,13 @@ async function eliminarTarea(idTareaEliminar) {
               <Row>
                 <Col>{objetoTarea.descripcion}</Col>
                 <Col>
-                  <Button>Ver Archivo</Button>
+                  <Button variant="secondary">Ver Archivo</Button>
                 </Col>
                 <Col>
-                  <Button onClick={() => eliminarTarea(objetoTarea.id)}>
+                  <Button
+                    variant="danger"
+                    onClick={() => eliminarTarea(objetoTarea.id)}
+                  >
                     Eliminar Tarea
                   </Button>
                 </Col>
